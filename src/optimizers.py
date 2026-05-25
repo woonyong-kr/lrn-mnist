eff5b5ee8dc9b26874825d0a9f0829ae802cd527
HyeYeonLee0112 <qquqqu26@naver.com>
2026-05-26 02:59:44 +0900

    fix: adam 버그 픽스

--- src/optimizers.py
+++ src/optimizers.py
@@ -77,7 +77,7 @@ class Adam:
             m, v는 실제 평균보다 작게 잡히는 경향이 있다. 이를 보정해야 한다.
             '''
             m_hat = self.m[key] / (1 - beta1*self.t)
-            v_hat = self.v[key] / (1 - beta2*self.t)
+            v_hat = self.v[key] / (1 - beta2**self.t)
 
             #가중치 업데이트
             params[key] =  params[key] - self.lr * m_hat / (np.sqrt(v_hat) + eps)
